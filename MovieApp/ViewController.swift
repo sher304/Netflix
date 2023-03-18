1a3c7fa95857e1a2a3ab6a30490713e35f919b9e
sher304 <esherovshermat@gmail.com>
2023-03-18 21:51:35 +0600

    :tada: Begin with Kingfisher, Snapkit, Alamofire, Hero

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Шермат Эшеров on 18/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
