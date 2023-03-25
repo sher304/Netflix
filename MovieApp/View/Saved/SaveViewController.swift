28f74c48a7299da426cdfba5f7bcd8bd04a95cea
sher304 <esherovshermat@gmail.com>
2023-03-25 16:32:14 +0600

    :lipstick: Done Tabbar with Custom Buttons

--- MovieApp/View/Saved/SaveViewController.swift
+++ MovieApp/View/Saved/SaveViewController.swift
@@ -12,7 +12,7 @@ class SaveViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .orange
+        view.backgroundColor = .blue
     }
 
 }
